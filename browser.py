cfc3e40cab98d4d4a457d6b3c1c1a18a2d811481
MahmudulHasan0101 <mahmudulhasan.cpp@gmail.com>
2025-12-23 19:58:06 -0900

    commit

--- browser.py
+++
@@ -1,5 +0,0 @@
-import webview
-
-def open(link, name=""):
-    webview.create_window(name, link, fullscreen=False)
-    webview.start()
--- temp.py
+++
@@ -1,5 +0,0 @@
-import webview
-
-webview.create_window('-', 'https://www.google.com/maps/search/?api=1&query=United+Hospital+Dhaka+Bangladesh', fullscreen=False)
-webview.start()
-
